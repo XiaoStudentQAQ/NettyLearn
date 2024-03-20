4491270e05b2515c7c09920f6a4fa19dafc6932f
jiakoukou <2572365788@qq.com>
2024-03-21 01:00:38 +0800

    读写事件

--- src/test/java/com/jiakoukou/nettylearn/nio/c4/WriteClient.java
+++ src/test/java/com/jiakoukou/nettylearn/nio/c4/WriteClient.java
@@ -5,6 +5,9 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
+/**
+ * 写事件客户端代码
+ */
 public class WriteClient {
     public static void main(String[] args) throws IOException {
         SocketChannel sc = SocketChannel.open();
@@ -13,6 +16,7 @@ public class WriteClient {
         // 3. 接收数据
         int count = 0;
         while (true) {
+            // 备注：这里的bytebuffer如果不是全局变量，则clear()毫无意义
             ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
             count += sc.read(buffer);
             System.out.println(count);
--- src/test/java/com/jiakoukou/nettylearn/nio/c4/WriteServer.java
+++ src/test/java/com/jiakoukou/nettylearn/nio/c4/WriteServer.java
@@ -7,6 +7,9 @@ import java.nio.channels.*;
 import java.nio.charset.Charset;
 import java.util.Iterator;
 
+/**
+ * 写事件服务端
+ */
 public class WriteServer {
     public static void main(String[] args) throws IOException {
         ServerSocketChannel ssc = ServerSocketChannel.open();
@@ -15,13 +18,17 @@ public class WriteServer {
         ssc.register(selector, SelectionKey.OP_ACCEPT);
         ssc.bind(new InetSocketAddress(8080));
         while (true) {
+            // select 方法, 没有事件发生，线程阻塞，有事件，线程才会恢复运行
+            // select 在事件未处理时，它不会阻塞, 事件发生后要么处理，要么取消，不能置之不理
             selector.select();
             Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
             while (iter.hasNext()) {
                 SelectionKey key = iter.next();
                 iter.remove();
                 if (key.isAcceptable()) {
+                    // 接收并获取客户端的channel
                     SocketChannel sc = ssc.accept();
+                    // 切换为非阻塞模式
                     sc.configureBlocking(false);
                     SelectionKey sckey = sc.register(selector, 0, null);
                     sckey.interestOps(SelectionKey.OP_READ);
@@ -32,19 +39,21 @@ public class WriteServer {
                     }
                     ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
 
-                    // 2. 返回值代表实际写入的字节数
+                    // 2. 返回值代表实际写入的字节数  写事件表示将buffer中的数据写到channel中
                     int write = sc.write(buffer);
                     System.out.println(write);
 
                     // 3. 判断是否有剩余内容
                     if (buffer.hasRemaining()) {
-                        // 4. 关注可写事件   1                     4
+                        // 4. 关注可写事件   1
+                        // 原来关注的事件 + 新加的读事件
                         sckey.interestOps(sckey.interestOps() + SelectionKey.OP_WRITE);
 //                        sckey.interestOps(sckey.interestOps() | SelectionKey.OP_WRITE);
                         // 5. 把未写完的数据挂到 sckey 上
                         sckey.attach(buffer);
                     }
                 } else if (key.isWritable()) {
+                    // 取出附件
                     ByteBuffer buffer = (ByteBuffer) key.attachment();
                     SocketChannel sc = (SocketChannel) key.channel();
                     int write = sc.write(buffer);
