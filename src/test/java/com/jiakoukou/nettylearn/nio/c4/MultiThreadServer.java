1d3d7b3f617ed85ecd8cbc51ecf651677b55d870
jiakoukou <2572365788@qq.com>
2024-03-22 00:42:27 +0800

    多线程优化读写事件

--- src/test/java/com/jiakoukou/nettylearn/nio/c4/MultiThreadServer.java
+++ src/test/java/com/jiakoukou/nettylearn/nio/c4/MultiThreadServer.java
@@ -12,7 +12,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.jiakoukou.nettylearn.nio.c2.ByteBufferUtil.debugAll;
 
-
+/**
+ * 多线程优化
+ */
 @Slf4j
 public class MultiThreadServer {
     public static void main(String[] args) throws IOException {
@@ -24,6 +26,9 @@ public class MultiThreadServer {
         bossKey.interestOps(SelectionKey.OP_ACCEPT);
         ssc.bind(new InetSocketAddress(8080));
         // 1. 创建固定数量的 worker 并初始化
+        // Runtime.getRuntime().availableProcessors() 如果工作在 docker 容器下，
+        // 因为容器不是物理隔离的，会拿到物理 cpu 个数，而不是容器申请时的个数
+        // 这个问题直到 jdk 10 才修复，使用 jvm 参数 UseContainerSupport 配置， 默认开启
         Worker[] workers = new Worker[Runtime.getRuntime().availableProcessors()];
         for (int i = 0; i < workers.length; i++) {
             workers[i] = new Worker("worker-" + i);
@@ -48,6 +53,10 @@ public class MultiThreadServer {
             }
         }
     }
+
+    /**
+     * worker负责读写
+     */
     static class Worker implements Runnable{
         private Thread thread;
         private Selector selector;
