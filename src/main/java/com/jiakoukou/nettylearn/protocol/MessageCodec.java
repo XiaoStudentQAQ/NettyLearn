9c3b377b54660df678f083fdff1e28e61c80011f
jiakoukou <2572365788@qq.com>
2024-04-07 00:11:39 +0800

    自定义协议

---
+++ src/main/java/com/jiakoukou/nettylearn/protocol/MessageCodec.java
@@ -0,0 +1,83 @@
+package com.jiakoukou.nettylearn.protocol;
+
+import com.jiakoukou.nettylearn.message.Message;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageCodec;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+@Slf4j
+@ChannelHandler.Sharable
+public class MessageCodec extends ByteToMessageCodec<Message> {
+
+    /**
+     * 出栈前的将自定义的message 编码成二进制，并写入 ByteBuf out
+     *
+     * @param ctx
+     * @param msg
+     * @param out
+     * @throws Exception
+     */
+    @Override
+    public void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) throws Exception {
+        // 1. 4 字节的魔数，例如java字节码的约定魔数是CoffeeBaby
+        out.writeBytes(new byte[]{1, 2, 3, 4});
+        // 2. 1 字节的版本,
+        out.writeByte(1);
+        // 3. 1 字节的序列化方式 jdk 0 , json 1
+        out.writeByte(0);
+        // 4. 1 字节的指令类型  【登录/登出/聊天等】
+        out.writeByte(msg.getMessageType());
+        // 5. 4 个字节  【请求序号，暂时用不上】
+        out.writeInt(msg.getSequenceId());
+        // 无意义，对齐填充 【满足最终长度是2的n次方倍】
+        out.writeByte(0xff);
+        // 6. 获取内容的字节数组
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos); // 将对象转换为字节数组
+        oos.writeObject(msg);
+        byte[] bytes = bos.toByteArray();
+        // 7. 长度
+        out.writeInt(bytes.length);
+        // 8. 写入内容
+        out.writeBytes(bytes);
+    }
+
+    /**
+     * 入栈后将二进制解码成自定义的message
+     *
+     * @param ctx
+     * @param in
+     * @param out
+     * @throws Exception
+     */
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        int magicNum = in.readInt();
+        byte version = in.readByte();
+        byte serializerType = in.readByte();
+        byte messageType = in.readByte();
+        int sequenceId = in.readInt();
+        in.readByte();
+        // 内容长度
+        int length = in.readInt();
+        byte[] bytes = new byte[length];
+        in.readBytes(bytes, 0, length);
+        // 判断序列化类型
+        if (serializerType == 0) {
+            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
+            Message message = (Message) ois.readObject();
+            log.debug("{}, {}, {}, {}, {}, {}", magicNum, version, serializerType, messageType, sequenceId, length);
+            log.debug("{}", message);
+            // 为了给下一个handle使用
+            out.add(message);
+        }
+    }
+}
--- src/test/java/com/jiakoukou/nettylearn/advance/c2/TestMessageCodec.java
+++ src/test/java/com/jiakoukou/nettylearn/advance/c2/TestMessageCodec.java
@@ -1,30 +1,42 @@
-//package com.jiakoukou.nettylearn.advance.c2;
-//
-//import io.netty.buffer.ByteBuf;
-//import io.netty.buffer.ByteBufAllocator;
-//import io.netty.channel.embedded.EmbeddedChannel;
-//import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-//import io.netty.handler.logging.LoggingHandler;
-//
-//public class TestMessageCodec {
-//    public static void main(String[] args) throws Exception {
-//        EmbeddedChannel channel = new EmbeddedChannel(
-//                new LoggingHandler(),
-//                new LengthFieldBasedFrameDecoder(
-//                        1024, 12, 4, 0, 0),
-//                new MessageCodec()
-//        );
-//        // encode
-//        LoginRequestMessage message = new LoginRequestMessage("zhangsan", "123", "张三");
-////        channel.writeOutbound(message);
-//        // decode
-//        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
-//        new MessageCodec().encode(null, message, buf);
-//
-//        ByteBuf s1 = buf.slice(0, 100);
-//        ByteBuf s2 = buf.slice(100, buf.readableBytes() - 100);
-//        s1.retain(); // 引用计数 2
-//        channel.writeInbound(s1); // release 1
-//        channel.writeInbound(s2);
-//    }
-//}
+package com.jiakoukou.nettylearn.advance.c2;
+
+import com.jiakoukou.nettylearn.message.LoginRequestMessage;
+import com.jiakoukou.nettylearn.protocol.MessageCodec;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * 自定义协议测试
+ */
+public class TestMessageCodec {
+    public static void main(String[] args) throws Exception {
+        EmbeddedChannel channel = new EmbeddedChannel(
+                new LoggingHandler(),
+                // 为什么我们规定了读取长度，还会出现粘包半包问题？
+                // 因为网卡规定，发送或接收的数据是有限制长度的，长度超过了，会自动给你截取，分成多次发送或接收，所以可能出现尿包和半包问题
+                // 即协议和硬件的限制最大数据长度限制和算法处理都会导致粘包、半包问题
+                // 解决粘包、半包问题；最大长度，长度偏移，长度占用字节，长度调整，剥离字节数
+                // 长度偏移=整个数-魔数长度=16-12
+                new LengthFieldBasedFrameDecoder(
+                        1024, 12, 4, 0, 0),
+                // 数据不完整不往下传播（不传播给下一个handler）
+                new MessageCodec()
+        );
+        // encode
+        LoginRequestMessage message = new LoginRequestMessage("zhangsan", "123", "张三");
+//        channel.writeOutbound(message);
+        // decode
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
+        new MessageCodec().encode(null, message, buf);
+
+        // 模拟半包现象
+        ByteBuf s1 = buf.slice(0, 100);
+        ByteBuf s2 = buf.slice(100, buf.readableBytes() - 100);
+        s1.retain(); // 引用计数 2
+        channel.writeInbound(s1); // release 1
+        channel.writeInbound(s2);
+    }
+}
